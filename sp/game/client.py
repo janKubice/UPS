3c5a253a74fe672c60e28e982ea3020600b0f4f0
Jan Kubice <jan.kubicer98@gmail.com>
2021-12-23 11:51:25 +0100

    update

--- sp/game/client.py
+++ sp/game/client.py
@@ -1,40 +1,220 @@
 import socket
 
+import msg_codes as codes
+
 class Client:
     def __init__(self, name) -> None:
         self.HOST = '127.0.0.1'  # The server's hostname or IP address
         self.PORT = 10000        # The port used by the server
         self.name = name
-        self.id = self.get_id()
+        self.id = -1
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.HOST, self.PORT))
+
+    def send_msg_to_server(self, code, msg):
+        """Odešle zprávu na server
+
+        Args:
+            code (int): kód zprávy
+            msg (string): dodatečné informace ke zprávě
+        """
+        self.socket.sendall(f'{self.id};{code};{msg}')
+
+    def receive_from_server(self):
+        """Poslouchá server a zjišťuje zda nebylo něco přijato
+        """
+        while(True):
+            try:
+                data = self.socket.recv(1024)
+            except:
+                print('něco špatně se serverem')
+                #TODO
+
+            if len(data) == 0:
+                continue
+
+            data = data.split(';')
+            msg_code = int(data[0])
+            msg_text = data[1]
+
+            if msg_code <= 0 or len(msg_text) <= 0:
+                continue
+
+            if msg_code == codes.GET_GAMES:
+                pass
+            elif msg_code == codes.SET_PIXEL:
+                self.receive_pixel(msg_text)
+            elif msg_code == codes.GET_ITEM:
+                self.receive_item(msg_text)
+            elif msg_code == codes.SEND_QUESS:
+                pass
+            elif msg_code == codes.CREATE_GAME:
+                pass
+            elif msg_code == codes.CONNECT_TO_GAME:
+                pass
+            elif msg_code == codes.RECONNECT:
+                pass
+            elif msg_code == codes.LEAVE:
+                pass
+            elif msg_code == codes.GET_POINTS:
+                pass
+            elif msg_code == codes.GET_ID:
+                self.receive_id(msg_text)
+
+    def receive_games(self, response:str):
+        """Vypíše běžící hry na obrazovku
+
+        Args:
+            response (str): text ze serveru obsahující hry ve tvaru p_hracu_1:max_hracu_1:id_mistnosti_1:stav_1,p_hracu_2:max_hracu_2:id_mistnosti_2:stav_2...
+            Jednolivé hry jsou odděleny pomocí , a jednotlivé údaje v jedné hře pomocí :
+        """
+        if response == None or response == '' or response == codes.ERR:
+            return
+
+        games = response.split(',')
+
+        for game in games:
+            game_info = game.split(':')
+            #TODO updatnout GUI a vypsat postupně všechny hry
+        pass
+
+    def receive_pixel(self, response:str):
+        """Namaluje pixel na obrazovku
+
+        Args:
+            response (str): odpověď ze serveru
+        """
+        if response == None or response == '' or response == codes.ERR:
+            return
+
+        params = response.split(',')
+        if len(params) is not 3:
+            return
+
+        #TODO aktualizovat hrací plochu
+
+    def receive_item(self, response:str):
+        """Ukáže jaké slovo má hráč malovat
+
+        Args:
+            response (str): odpověď ze serveru co má hráč malovat
+        """
+        if response == None or response == '' or response == codes.ERR:
+            return
+        
+        #TODO ukázat slovo hráči
+
+    def receive_quess_response(self, response:str):
+        if response == None or response == '' or response == codes.ERR:
+            return
+
+        if response == 't':
+            #TODO ukázat hráči že uhodl
+            pass
+        else:
+            #TODO ukázat hráči, že neuhodl
+            pass
+
+    def receive_create_game_response(self, response:str):
+        """Zjistí zda se povedla založit hra
+
+        Args:
+            # response (str): odpověď ze serveru ve formátu response_CODE,id_mistnosti
+        """
+        if response == None or response == '' or response == codes.ERR:
+            return
+
+        params = response.split(',')
+        if len(params) is not 2:
+            return
+
+        if params[0] == codes.ERR:
+            #TODO Nepovedlo se
+            pass
+        else:
+            #TODO povedlo se a založit, ukázat zakladeli id a tak
+            pass
+
+    def receive_connect_response(self, response:str):
+        """Připojí hráče do hry pokud může
+
+        Args:
+            response (str): odpověď se serveru, obsahuje zda se povedlo připojit, pokud ano obsahuje dále údaje o hře
+        """
+        if response == None or response == '' or response == codes.ERR:
+            return
+        
+        params = response.split(',')
+
+        #TODO Přizpůsobit GUI
 
 
-    def send_test(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.HOST, self.PORT))
-            s.sendall(b'Hello, world')
-            data = s.recv(1024)
+    def receive_reconnect_response(self, response:str):
+        """Připojí hráče do hry, přepne obrazovku a zobrazí hru v jajím stavu
 
-        print('Received', repr(data))
+        Args:
+            response (str): zpráva ze serveru
+        """
+        if response == None or response == '' or response == codes.ERR:
+            return
 
-    def get_games(self):
-        """Vrátí aktuální hry ke kterým se lze připojit ve formátu pole odděleného ; :
-            id, name, p_max, p_actual, id, name....
+        #TODO vymyslet co je potřeba přijmout
+
+
+    def receive_leave_response(self, response:str):
+        if response == None or response == '':
+            return
+
+        code = int(response)
+        if code == codes.OK:
+            #TODO exit a tak
+            pass
+        else:
+            #TODO zkusit znovu a zatím neodpojovat
+            pass
+
+    def receive_get_points(self, response:str):
+        if response == None or response == '':
+            return
+
+        points = [int(numeric_string) for numeric_string in response.split(',')]
+
+        if len(points) <= 1:
+            return
+
+        #TODO update GUI
+
+    def receive_id(self, response:str):
+        """Nastaví id hráče
+
+        Args:
+            response (str): přijatá zpráva ze serveru
+        """
+        if response == None or response == '':
+            return
+        
+        id = int(response)
+        if id <= 0:
+            return
+
+        self.id = id
+
+    def send_get_games(self):
+        """Zeptá se serveru na aktuální hry do kterých se lze připojit
         """
         pass
 
-    def send_pixel(self, x: int , y:int) -> bool:
-        """Odešle na server požadavek na vyplnění pixelu, pokud projde vrátí True.
+    def send_pixel(self, x: int , y:int, color:str):
+        """Odešle na server požadavek na vyplnění pixelu
 
         Args:
             x (int): x pixelu
             y (int): y pixelu
-
-            Returns:
-                bool: jestli lze pixel vyplnit
+            color (str): barva pixelu
         """
         pass
 
-    def get_item_to_draw(self) -> str:
+    def send_get_item_to_draw(self):
         """Dotáže se serveru na obrázek ke kreslení
 
         Returns:
@@ -42,32 +222,34 @@ class Client:
         """
         pass
 
-    def send_quess(self, quess: str) -> bool:
+    def send_quess(self, quess: str):
         """Odešle na server tip toho co si hráč myslí, pokud je to správně vrací True jinak False
 
         Args:
             quess (str): odhad hráče
-
-        Returns:
-            bool: zda je tip dobře
         """
         pass
 
-    def send_create_new_game(self) -> bool:
-        pass
-
-    def get_points(self) -> int:
-        """Dotáže se serveru na počet bodu hráče
-
-        Returns:
-            int: počet bodů
+    def send_create_new_game(self):
+        """Odešle požadavek k vytvoření nové hry
         """
         pass
 
-    def get_id(self) -> int:
+    def send_connect_to_game(self):
+        pass
+
+    def send_reconnect_to_game(self):
+        pass
+
+    def send_leave(self):
+        pass
+
+    def send_get_points(self):
+        """Dotáže se serveru na počet bodu hráčů
+        """
+        pass
+
+    def send_get_id(self):
         """Získá od serveru id hráče
-
-        Returns:
-            int: id hráče
         """
         pass
---
+++ sp/game/gui.py
@@ -0,0 +1,255 @@
+import pygame
+from tkinter import *
+import grid
+from grid import colorPallet
+from grid import pixelArt
+from grid import menu
+from grid import grid
+import sys
+
+class Gui:
+    def __init__(self) -> None:
+        sys.setrecursionlimit(1000000)
+        pygame.init()
+
+        self.paintBrush = pygame.image.load("sp/game/art/Paintbrush.png")
+        self.currentVersion = 1.1
+
+        #Defaultní hodnoty
+        self.rows = 50
+        self.cols = 50
+        self.wid = 600
+        self.heigh = 600
+
+        self.client = None
+        self.checked = []
+
+    def fill(self, spot, grid, color, c):
+        if spot.color != c:
+            pass
+        else:
+            spot.click(grid.screen, color)
+            pygame.display.update()
+            
+            i = spot.col 
+            j = spot.row 
+
+            if i < self.cols-1: #pravá
+                self.fill(grid.getGrid()[i + 1][j], grid, color, c)
+            if i > 0: #levá
+                self.fill(grid.getGrid()[i - 1][j], grid, color, c)
+            if j < self.rows-1: #nahoru
+                self.fill(grid.getGrid()[i][j + 1], grid, color, c)
+            if j > 0 : #dolů
+                self.fill(grid.getGrid()[i][j - 1], grid, color, c)
+
+    def changeCaption(self, txt):
+        pygame.display.set_caption(txt)
+
+
+    def initalize_game(self, cols, rows, showGrid=False):
+        """
+        Vytvoří obrazovku hry
+        """
+        global pallet, grid, win, tools, lineThickness, saveMenu
+
+        try:
+            del grid
+        except:
+            pass
+        
+        pygame.display.set_icon(self.paintBrush)   
+        win = pygame.display.set_mode((int(self.wid), int(self.heigh) + 100))
+        pygame.display.set_caption('Untitled')
+        win.fill((255,255,255))
+
+        grid = pixelArt(win, int(self.wid), int(self.heigh), cols, rows, showGrid)
+        grid.drawGrid()
+
+        pallet = colorPallet(win, 90, 90, 3, 3, True, 10, grid.height + 2)
+        pallet.drawGrid()
+
+        colorList = [(0,0,0), (255,255,255), (255,0,0), (0,255,0), (0,0,255), (255,255,0), (255,168,0), (244, 66, 173), (65, 244, 226)]
+        pallet.setColor(colorList)
+
+        tools = menu(win, 200, 40, 5, 1, True, grid.width - 210, grid.height + 50)
+        tools.drawGrid()
+
+        buttons = ['D', 'E', 'F', 'R', 'C']
+        tools.setText(buttons)
+        tools.drawGrid()
+
+        l = tools.getGrid()
+        l[0][0].show(grid.screen, (255,0,0),1, True)
+
+        lineThickness = menu(win, 180, 40, 4, 1, True, grid.width - 200, grid.height + 10)
+        lineThickness.drawGrid()
+
+        buttons = ['1', '2', '3', '4']
+        lineThickness.setText(buttons)
+
+        pygame.display.update()
+
+    def draw_menu(self):
+        pygame.display.set_icon(self.paintBrush)   
+        win = pygame.display.set_mode((int(self.wid), int(self.heigh) + 100))
+        pygame.display.set_caption('Menu')
+        win.fill((255,255,255))
+
+        color_white = (255,255,255)
+        color_light = (170,170,170)
+        color_dark = (100,100,100)
+        
+        text_font = pygame.font.SysFont('Corbel',35)
+        text = text_font.render('Play' , True , color_white)
+        
+        while True:
+            for ev in pygame.event.get():
+            
+                if ev.type == pygame.QUIT:
+                    pygame.quit()
+                    
+                #ověření jestli se klikla myš
+                if ev.type == pygame.MOUSEBUTTONDOWN:
+                    
+                    #je kliknutí v pozici tlačítka?
+                    if self.wid/2 <= mouse[0] <= self.wid/2+140 and self.heigh/2 <= mouse[1] <= self.heigh/2+40:
+                        self.gameloop()
+                        
+            win.fill((60,25,60))
+            mouse = pygame.mouse.get_pos()
+            
+            #hover přes tlačítko
+            if self.wid/2 <= mouse[0] <= self.wid/2+140 and self.heigh/2 <= mouse[1] <= self.heigh/2+40:
+                pygame.draw.rect(win,color_light,[self.wid/2,self.heigh/2,140,40])
+            else:
+                pygame.draw.rect(win,color_dark,[self.wid/2,self.heigh/2,140,40])
+            
+
+            win.blit(text , (self.wid/2+50,self.heigh/2))
+            pygame.display.update()
+
+    def gameloop(self):
+        global pallet
+
+        self.initalize_game(self.cols, self.rows, False)
+        pygame.display.update()
+
+        color = (0,0,0) 
+        thickness = 1
+        replace = False
+        doFill = False
+
+        run = True
+        while run:
+            ev = pygame.event.get()
+
+            for event in ev:
+                if event.type == pygame.QUIT:
+                    window = Tk()
+                    window.withdraw()
+                    
+                    run = False
+
+                if pygame.mouse.get_pressed()[0]: 
+                    try:
+                        pos = pygame.mouse.get_pos()
+                        if pos[1] >= grid.height: 
+                            if pos[0] >= tools.startx and pos[0] <= tools.startx + tools.width and pos[1] >= tools.starty and pos[1] <+ tools.starty + tools.height: #If the mouse ic clicking on the tools grid
+                                replace = False
+                                doFill = False
+                                tools.drawGrid() 
+                                buttons = ['D', 'E', 'F', 'R', 'C']
+                                tools.setText(buttons)
+                                
+                                clicked = tools.clicked(pos)
+                                clicked.show(grid.screen, (255,0,0), 1, True)
+
+                                if clicked.text == 'D': #Draw  
+                                    color = (0,0,0)
+                                elif clicked.text == 'E': #Erase
+                                    color = (255,255,255)
+                                elif clicked.text == 'F':# Fill
+                                    doFill = True
+                                elif clicked.text == 'R':# Replace
+                                    replace = True
+                                elif clicked.text == 'C':# Clear 
+                                    grid.clearGrid()
+                                    tools.drawGrid() 
+                                    buttons = ['D', 'E', 'F', 'R', 'C']
+                                    tools.setText(buttons)
+                                    l = tools.getGrid()
+                                    l[0][0].show(grid.screen, (255,0,0),1, True)
+
+                            elif pos[0] >= pallet.startx and pos[0] <= pallet.startx + pallet.width and pos[1] >= pallet.starty and pos[1] <= pallet.starty + pallet.height:
+                                clicked = pallet.clicked(pos)
+                                color = clicked.getColor() 
+
+                                pallet = colorPallet(win, 90, 90, 3, 3, True, 10, grid.height + 2)
+                                pallet.drawGrid()
+
+                                colorList = [(0,0,0), (255,255,255), (255,0,0), (0,255,0), (0,0,255), (255,255,0), (255,168,0), (244, 66, 173), (65, 244, 226)]
+                                pallet.setColor(colorList)
+                                clicked.show(grid.screen, (255,0,0), 3, True)
+
+                            elif pos[0] >= lineThickness.startx and pos[0] <= lineThickness.startx + lineThickness.width and pos[1] >= lineThickness.starty and pos[1] <= lineThickness.starty + lineThickness.height:
+                                lineThickness.drawGrid()
+                                buttons = ['1', '2', '3', '4']
+                                lineThickness.setText(buttons)
+                                
+                                clicked = lineThickness.clicked(pos)
+                                clicked.show(grid.screen, (255,0,0), 1, True)
+
+                                thickness = int(clicked.text) 
+                        
+                        else:
+                            if replace:
+                                tools.drawGrid()
+                                buttons = ['D', 'E', 'F', 'R', 'C']
+                                tools.setText(buttons)
+                                
+                                tools.getGrid()[0][0].show(grid.screen, (255,0,0), 1, True)
+
+                                clicked = grid.clicked(pos)
+                                c = clicked.color
+                                replace = False
+
+                                for x in grid.getGrid():
+                                    for y in x:
+                                        if y.color == c:
+                                            y.click(grid.screen, color)
+                                            print(y)
+                            
+                            elif doFill:
+                                clicked = grid.clicked(pos)
+                                if clicked.color != color:
+                                    self.fill(clicked, grid, color, clicked.color)
+                                    pygame.display.update()
+
+                            else:
+                                name = pygame.display.get_caption()[0]
+                                if name.find("*") < 1:
+                                    self.changeCaption(name + '*')
+
+                                clicked = grid.clicked(pos)
+                                print(clicked)
+                                clicked.click(grid.screen,color)
+                                if thickness == 2:
+                                    for pixel in clicked.neighbors:
+                                        pixel.click(grid.screen, color)
+                                elif thickness == 3:
+                                    for pixel in clicked.neighbors:
+                                        pixel.click(grid.screen, color)
+                                        for p in pixel.neighbors:
+                                            p.click(grid.screen, color)
+                                elif thickness == 4:
+                                    for pixel in clicked.neighbors:
+                                        pixel.click(grid.screen, color)
+                                        for p in pixel.neighbors:
+                                            p.click(grid.screen, color)
+                                            for x in p.neighbors:
+                                                x.click(grid.screen, color)
+
+                        pygame.display.update()
+                    except AttributeError:
+                        pass
