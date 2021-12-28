eb3d4ea34beb3f1d2ac3d463a8bbd6fc14648871
janKubice <60101148+janKubice@users.noreply.github.com>
2021-12-28 15:57:14 +0100

    update

--- sp/game/main.py
+++ sp/game/main.py
@@ -1,10 +1,13 @@
 import pygame
 from gui import Gui
+from client import Client
+import threading
 
 if __name__ == '__main__':
-    #client = Client('Pepa')
-    #thread = threading.Thread(target=client.receive_from_server)
-    #thread.start()
+    client = Client('Pepa')
+    client.send_msg_to_server(1,"ahoj servere")
+    thread = threading.Thread(target=client.receive_from_server)
+    thread.start()
     application = Gui()
     application.draw_menu()
     pygame.quit()
